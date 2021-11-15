23602aa0f82dd28a21ff0e5627d8b08a607b8ab8
Brian Antunes <brian.antunes@yahoo.com>
2021-11-15 10:13:41 -0500

    Add project files.

---
+++ WeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WeatherForecast.Models;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ActionResult Weather()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public String WeatherDetail(string City)
+        {
+            string appId = "";
+
+            string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&units=imperial&cnt=1&APPID={1}", City, appId);
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+                RootObject weatherInfo = JsonSerializer.Deserialize<RootObject>(json);
+                ResultViewModel rslt = new ResultViewModel();
+
+                rslt.Country = weatherInfo.sys.country;
+                rslt.City = weatherInfo.name;
+                rslt.Lat = Convert.ToString(weatherInfo.coord.lat);
+                rslt.Lon = Convert.ToString(weatherInfo.coord.lon);
+                rslt.Description = weatherInfo.weather[0].description;
+                rslt.Humidity = Convert.ToString(weatherInfo.main.humidity);
+                rslt.Temp = Convert.ToString(weatherInfo.main.temp);
+                rslt.TempFeelsLike = Convert.ToString(weatherInfo.main.feels_like);
+                rslt.TempMax = Convert.ToString(weatherInfo.main.temp_max);
+                rslt.TempMin = Convert.ToString(weatherInfo.main.temp_min);
+                rslt.WeatherIcon = weatherInfo.weather[0].icon;
+
+                //Converting OBJECT to JSON String   
+                var jsonstring = JsonSerializer.Serialize(rslt);
+
+                //Return JSON string.  
+                return jsonstring;
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new Models.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
